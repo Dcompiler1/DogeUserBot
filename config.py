65420d56c88802ebbf40cad2b673aca62a737e1a
Dcompiler1 <104096245+Dcompiler1@users.noreply.github.com>
2023-03-18 11:15:46 +0500

    Update and rename exampleconfig.py to config.py

--- config.py
+++ config.py
@@ -11,18 +11,18 @@ from sample_config import Config
 
 class Development(Config):
     # Get this values from the https://my.telegram.org/apps
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 15293388
+    API_HASH = "5942c84494f1c8e1e8ac73080c8c7205"
     # The name to display in your alive message
-    ALIVE_NAME = "Your name"
+    ALIVE_NAME = "Shukurullayev"
     # Create any PostgreSQL database (recommend to use ElephantSQL) and paste that link here
-    DB_URI = "Your DB url"
+    DB_URI = "postgres://torlbsbj:Bm3D6fgyiqUG7FIkww-sU84LH1I5G9xS@mel.db.elephantsql.com/torlbsbj"
     # Get this value by running https://bit.do/DogessRepl
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1ApWapzMBu5lSl_zeucJuROcCBgQ3pOb3rZ-Lvvs2xSvTe3rskJtsX-F3v25CYZNiLwQ7-krDhjVn1LzkptYbaSo0yujSmNEZnuJJCBeqIvoUvKJSYmknWT395SELP_DglTtUk5KLi2q-jUWPfXIadgIp-aX5bGWsQ2hSfzm7NXFrpJ_AgmMl8sFZoi-GWAiEYWYk26cZxo1MYXw2t15tFZ_xzLf7jNWzqbDmOr-Sf2xw9fNSwDlcRiFQwFuET4hsy7akSdtUo9gFu88TsmhuzlwXV7jHhAoTrfB7WAkYThdmKKNIjAemSUMCzOVmyRuomHPpz80D3xzfECAZWJkIwnunnzH4rjw="
     # Create a new bot in @BotFather ( https://t.me/BotFather ) and fill the following vales with Bot TokenAPI
-    BOT_TOKEN = "Your value"
+    BOT_TOKEN = "1717885125:AAH0LVoCMCpZ348tuCP4pQZ9yxuryafBNoA"
     # Create a private group and a @MissRose_bot to it and type /id and paste that Chat ID here (replace that -100 with that group ID)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = -1001522836300
     # Command handler
     CMDSET = "."
     # Command hanler for sudo
